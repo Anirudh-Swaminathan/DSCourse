d779e7aac1b28d89317423edfba17865a4f0fdc3
Anirudh Swaminathan <aniswami97@gmail.com>
2017-04-21 11:39:47 +0530

    [ADD] Dijkstra's algorithm for a Directed Graph using Adjacency Matrix [MODIFY] Prettified the output of Prims Algorithm.

--- Assignment-4/dijkstra_directed.cpp
+++ Assignment-4/dijkstra_directed.cpp
@@ -3,24 +3,21 @@
 	Anirudh Swaminathan 102115004 - Chemical 2nd year - 4th semester
 	Abhinandan Nabera 102115038 - Chemical 2nd year - 4th semester
 
-	This code is for implementing Prims algorithm for Minimum Spanning Tree. 
-	This is for an undirected graph.
+	This code is for implementing Dijkstra's algorithm for single source shortest path algorithm. 
+	This is for a directed graph.
 
 	This is a part of the programming assignment 4 given to students who opted to do Minors in CS in NIT-Trichy
 */
 #include <iostream>
-#include <queue>
 using namespace std;
 #define INF 1000000
-
 int gr[10][10];
 bool mark[10];
 int n;
 int m;
 int dist[10];
-int parent[10];
 
-void prim(int s)
+void dijkstra(int s)
 {
     dist[s-1]=0;
     for(int co=0; co<n-1; ++co)
@@ -38,17 +35,19 @@ void prim(int s)
         mark[ind]=1;
         for(int i=0; i<n; ++i)
         {
-            if(mark[i]==0 && gr[ind][i]!=0 && gr[ind][i]<dist[i]) {
-				dist[i]=gr[ind][i];
-				parent[i] = ind;
-			}
+            if(mark[i]==0 && gr[ind][i]!=0 && dist[ind]+gr[ind][i]<dist[i]) dist[i]=dist[ind]+gr[ind][i];
         }
     }
 }
 
 int main()
 {
-    cout<<"\nEnter the number of nodes: ";
+    int t;
+    cout<<"\nEnter the number of test cases: ";
+    cin>>t;
+    for(int w=0; w<t; ++w)
+    {
+        cout<<"\nEnter the number of nodes: ";
         cin>>n;
         cout<<"\nEnter the number of edges: ";
         cin>>m;
@@ -56,7 +55,7 @@ int main()
         for(int i=0; i<n; ++i) dist[i]=INF;
         for(int i=0; i<n; ++i)
             for(int j=0; j<n; ++j) gr[i][j]=0;
-        cout<<"\nEnter "<<m<<" edges 1 by 1\n";
+        cout<<"\nEnter "<<m<<" edges 1 by 1 (Format: From To Weight)\n";
         for(int i=0; i<m; ++i)
         {
             int a,b,r;
@@ -66,36 +65,33 @@ int main()
             if(gr[a-1][b-1]==0)
             {
                 gr[a-1][b-1]=r;
-                gr[b-1][a-1]=r;
             }
             else
             {
                 if(gr[a-1][b-1]>r)
                 {
                     gr[a-1][b-1]=r;
-                    gr[b-1][a-1]=r;
                 }
             }
         }
         int s;
         cout<<"\nEnter the source node: ";
         cin>>s;
-        prim(s);
-                cout<<"\nOutputting shortest weight edges of the MST...\n";
-                int sum=0;
+        dijkstra(s);
+        cout<<"\nOutputting shortest distances from the source...\n";
         for(int i=0; i<n; ++i)
         {
-			if(i+1!=s){
-				if(dist[i]!=INF)
-				{
-					cout<<parent[i]+1<<"-"<<i+1<<" : "<<dist[i]<<"\n";
-					sum+=dist[i];
-				}
-				else cout<<-1<<" ";
+            if(i+1!=s){
+                if(dist[i]!=INF)
+                    cout<<dist[i]<<" ";
+                else cout<<-1<<" ";
+            }
+            else
+            {
+				cout<<"s ";
 			}
         }
-        cout<<"The cost of the Minimum Spanning Tree is "<<sum<<"\n";
         cout<<"\n";
-        return 0;
+    }
+    return 0;
 }
-
